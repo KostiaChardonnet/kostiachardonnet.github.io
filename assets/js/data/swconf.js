da81aeefa11b5f381cf44b1da2bcd7cf9636bb63
KostiaChardonnet <kostia.chardonnet@yahoo.fr>
2026-07-23 17:52:45 +0200

    qtrs

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1784648164',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/publications/', '/activities/', '/teaching/', '/contact/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1784821933',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/publications/', '/activities/', '/teaching/', '/contact/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
